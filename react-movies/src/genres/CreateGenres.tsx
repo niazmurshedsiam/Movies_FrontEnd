f679e1f95eb4580774b4e2c687770015e5b444f5
Niaz Murshed Siam <niazmurshed16@gmail.com>
2023-08-02 00:12:21 +0600

    Using JavaScript for Navigation

--- react-movies/src/genres/CreateGenres.tsx
+++ react-movies/src/genres/CreateGenres.tsx
@@ -1,7 +1,12 @@
+import { useHistory } from "react-router";
+import Button from "../utils/Button/Button";
+
 export default function CreateGenres() {
+    const history = useHistory();
     return (
         <>
             <h3>Create Genres</h3>
+            <Button onClick={() => { history.push("/genres") }}>Save Change</Button>
         </>
     )
 }
--- react-movies/src/utils/Button/Button.tsx
+++ react-movies/src/utils/Button/Button.tsx
@@ -1,9 +1,10 @@
 export default function Button(props: ButtonProps) {
 
-    return <button className="btn btn-primary">{props.children}</button>
+    return <button className="btn btn-primary" onClick={props.onClick}>{props.children}</button>
 
 }
 
 interface ButtonProps {
-    children: React.ReactNode
+    children: React.ReactNode,
+    onClick(): void
 }
